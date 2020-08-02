4fc1725c88d4684df6e0f2f6b399b41142d74a3f
Gregory Eales <gregoeales@aol.com>
2020-08-02 14:32:42 +0100

    Added main and lib

---
+++ lib/primes.py
@@ -0,0 +1,35 @@
+import math
+from tqdm import tqdm
+
+
+def get_primes(n):
+
+	primes = [2]
+
+	for i in tqdm(range(3, n), "Getting Primes"):
+		
+		is_prime = True
+
+		for p in primes:
+		
+			if i % p == 0:
+				is_prime = False
+				break
+			
+			if p > math.sqrt(i):
+				is_prime = True
+				break
+		
+		if is_prime:
+			primes.append(i)
+
+	return primes
+
+if __name__ == "__main__":
+	primes = get_primes(int(math.sqrt(600851475143)))
+	for p in reversed(primes):
+		if 600851475143 % p == 0:
+			print(p)
+			break
+
+
