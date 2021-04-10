536e401aca136186fc6b926edb51c4b894a13e78
Gregory H Eales <gregoeales@aol.com>
2021-04-10 11:24:12 -0700

    clean tests

--- lib/tests.py
+++ lib/tests.py
@@ -1,32 +1,18 @@
+message = "algorithm failure @ f({}): got {} instead of {}"
 
-
+def test_set(a, b, f):
+	r = f(a)
+	if r != b:
+		print(message.format(a, r, b))
 
 def test_f(f):
+
 	print("-"*85)
-	r = f(10)
-	if r != 17:
-		return "Algorithm Failure @ f({}): got {} instead of {}".format(10, r, 17)
-	
-	r = f(15)
-	if r != 46:
-		return "Algorithm Failure @ f({}): got {} instead of {}".format(15, r, 46)
-	
-	r = f(20)
-	if r != 86:
-		return "Algorithm Failure @ f({}): got {} instead of {}".format(20, r, 86)
-	
-	"""	
-	if f(30) != 213:
-		return "Algorithm Failure @ f({})".format(30)
-	
-	
-	if f(100) != 2918:
-		return "Algorithm Failure @ f({})".format(100)
-	
-	
-	if f(5000) != 11134074:
-		return "Algorithm Failure @ f({})".format(5000)
 
-	"""
+	pairs = [[10, 17], [15, 46], [20, 86], [30, 213], [100, 2918], [5000, 11134074]]
 
-	return "Algorithm Success"
+	for pair in pairs:
+		test_set(pair[0], pair[1], f)
+
+
+	print("-"*85)
